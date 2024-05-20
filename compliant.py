0f7294f044e2ed06b69e2d6bdf80aefc909f6161
Jujutsualfayo <Alphaspaam@gmail.com>
2024-05-20 02:14:54 -0700

    My first apllication

--- compliant.py
+++ compliant.py
@@ -1,10 +1,24 @@
-def calculate_simple_interest(principal, rate, time):
-    interest = (principal * rate * time) / 100
-    return interest
+import uuid
+from datetime import datetime
+import json
+import os
 
+class BaseModel:
+    objects = {}
 
-principal = 1000
-rate = 5
-time = 2
-interest = calculate_simple_interest(principal, rate, time)
-print(f"The simple interest is: {interest}")
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id', str(uuid.uuid4()))
+        self.created_at = kwargs.get('created_at', datetime.now().isoformat())
+        self.updated_at = kwargs.get('updated_at', self.created_at)
+        self.__class__.objects[self.id] = self
+
+    def save(self):
+        self.updated_at = datetime.now().isoformat()
+        with open('storage.json', 'w') as file:
+            json.dump(self.to_dict(), file)
+
+    def to_dict(self):
+        return {
+                'id': self.id,
+                'created_at': self.created
+
