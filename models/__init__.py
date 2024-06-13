3e71da5213f6a9652ddfbc99c9cdd55cb42bb346
Jujutsualfayo <Alphaspaam@gmail.com>
2024-06-13 03:37:40 -0700

    Airbnb console

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""__init__ method for models package, or
-Module for FileStorage autoinit."""
-
+"""__init__ magic method for my models directory"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python3
-""" Module Class Amenity """
-
+#!/usr/bin/python3
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """ Class Amenity """
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
     name = ""
